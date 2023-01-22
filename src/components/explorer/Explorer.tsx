b75cd9253f6fe7efb15c9ac77481c059ccccc925
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2023-01-22 08:46:30 -0500

    Loaded blockchain in explorer

--- src/components/explorer/Explorer.tsx
+++ src/components/explorer/Explorer.tsx
@@ -1,29 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@mui/styles';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Input from '@mui/material/Input';
-import FormControl from '@mui/material/FormControl';
-import FormHelperText from '@mui/material/FormHelperText';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@mui/styles";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Input from "@mui/material/Input";
+import FormControl from "@mui/material/FormControl";
+import FormHelperText from "@mui/material/FormHelperText";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
 
-import { invoke } from '@tauri-apps/api';
-import Typography from '@mui/material/Typography';
+import { invoke } from "@tauri-apps/api";
+import Typography from "@mui/material/Typography";
 
 const SendFile = () => {
   const [blocks, setBlocks] = useState<any>(null);
 
   useEffect(() => {
-    invoke('get_blocks').then((rblocks) => {
-      let blocks = rblocks as string;
-      blocks = blocks.split('\n')[0];
-      setBlocks(JSON.parse(blocks));
-    });
+    fetch("src/assets/blockchain.json")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setBlocks(data);
+      });
   }, []);
 
   return (
@@ -32,19 +33,25 @@ const SendFile = () => {
         blocks.map((block: any) => (
           <Card sx={{ minWidth: 275, marginBottom: 1 }}>
             <CardContent>
-              <Typography variant='h5'>Index: {block.index}</Typography>
-              <Typography variant='body2'>
+              <Typography variant="h5">Index: {block.index}</Typography>
+              <Typography variant="body2">
                 Timestamp: {block.timestamp}
               </Typography>
-              <Typography variant='body2'>Sender: {block.sender}</Typography>
-              <Typography variant='body2'>
-                Receiver: {block.receiver}
+              <Typography variant="body2">Sender: {block.sender_pub_key}</Typography>
+              <Typography variant="body2">
+                Receiver: {block.receiver_pub_key}
               </Typography>
-              <Typography variant='body2'>
+              <Typography variant="body2">
                 File hash: {block.file_hash}
               </Typography>
-              <Typography variant='body2'>
-                Previous hash: {block.prev_hash}
+              <Typography variant="body2">
+                File hash: {block.sender_signature}
+              </Typography>
+              <Typography variant="body2">
+                Previous hash: {block.prev_block_hash}
+              </Typography>
+              <Typography variant="body2">
+                Block hash: {block.block_hash}
               </Typography>
             </CardContent>
           </Card>
